58c9f6ea9d6b952140766c6d741d1010ea15f3cc
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-09-18 22:14:27 -0300

    Criando classes de modelo e usando no cadastro de lançamentos

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Constants } from './../../shared/Constants';
 import { CategoriaService } from 'src/app/categorias/categoria.service';
 import { PessoaService } from './../../pessoas/pessoa.service';
 import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { Lancamento } from './../../core/model';
 
 @Component({
   selector: 'app-lancamento-cadastro',
@@ -21,6 +23,7 @@ export class LancamentoCadastroComponent implements OnInit {
 
   categorias = [];
   pessoas = [];
+  lancamento = new Lancamento();
 
   constructor(
     private categoriaService: CategoriaService,
@@ -34,13 +37,17 @@ export class LancamentoCadastroComponent implements OnInit {
     this.carregarPessoas();
   }
 
-  carregarCategorias() {
+  salvar(form: NgForm) {
+    console.log(this.lancamento);
+  }
+
+  private carregarCategorias() {
     return this.categoriaService.listarTodas()
       .then(categorias => this.categorias = categorias.map(c => ({ label: c.nome, value: c.codigo })))
       .catch(erro => this.errorHandler.handle(erro));
   }
 
-  carregarPessoas() {
+  private carregarPessoas() {
     return this.pessoaService.listarTodas()
       .then(pessoas => this.pessoas = pessoas.map(p => ({ label: p.nome, value: p.codigo })))
       .catch(erro => this.errorHandler.handle(erro));
