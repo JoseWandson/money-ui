c82ee0c4fcf276b1c7bb40de72be6e1e2ed6f23c
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-09-20 23:09:08 -0300

    Implementando o cadastro de pessoas

--- src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { MessageService } from 'primeng/api';
+
+import { PessoaService } from '../pessoa.service';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { Pessoa } from './../../core/model';
 
 @Component({
   selector: 'app-pessoa-cadastro',
   templateUrl: './pessoa-cadastro.component.html',
   styleUrls: ['./pessoa-cadastro.component.css']
 })
-export class PessoaCadastroComponent implements OnInit {
+export class PessoaCadastroComponent {
 
-  constructor() { }
+  pessoa = new Pessoa();
 
-  ngOnInit(): void {
+  constructor(
+    private pessoaService: PessoaService,
+    private messageService: MessageService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  salvar(form: NgForm) {
+    this.pessoaService.adicionar(this.pessoa)
+      .then(() => {
+        this.messageService.add({ severity: 'success', detail: 'Pessoa adicionada com sucesso!' });
+
+        form.reset();
+        this.pessoa = new Pessoa();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
 }
--- src/app/pessoas/pessoa.service.ts
+++ src/app/pessoas/pessoa.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Pessoa } from './../core/model';
+
 export class PessoaFiltro {
   nome: string;
   pagina = 0;
@@ -61,5 +63,14 @@ export class PessoaService {
     return this.http.put(`${this.pessoasUrl}/${codigo}/ativo`, ativo, { headers })
       .toPromise()
       .then(() => null);
+  }
+
+  adicionar(pessoa: Pessoa): Promise<Pessoa> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AbW9uZXkuY29tOmFkbWlu')
+      .append('Content-Type', 'application/json');
+
+    return this.http.post<Pessoa>(this.pessoasUrl, pessoa, { headers })
+      .toPromise();
   }
 }
